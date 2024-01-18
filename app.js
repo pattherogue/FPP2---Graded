4b8e6d17c1cbf95a56872815b5ceba8f99521b4e
Paolo Gomez <peweegardner248@gmail.com>
2024-01-18 11:14:31 -0800

    Update app.js

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ function encrypt (message, shiftValue)
     encryptedMessage = ""
     //keep track of the number of processed letter
     letterCounter = 0
-//Loop through each character in message
-    //for each character in message:
-        //if character is in the alphabet:
-            //Shift the character
+    
+    function shiftCharacterToRight() {
+        
+    }
+
+
   // Your encryption code here
   return encryptedMessage;
 }
