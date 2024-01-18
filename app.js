bc69682fb38007b45ca042655ba298d830224e73
Paolo Gomez <peweegardner248@gmail.com>
2024-01-18 11:06:23 -0800

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,15 @@
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 function encrypt (message, shiftValue)
-{
+//Initialize Variables
+{   //empty string to store final result
     encryptedMessage = ""
+    //keep track of the number of processed letter
     letterCounter = 0
+//Loop through each character in message
+    //for each character in message:
+        //if character is in the alphabet:
+            //Shift the character
   // Your encryption code here
   return encryptedMessage;
 }
